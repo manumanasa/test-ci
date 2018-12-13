b13b0fe6d4a29e0f72df9c091d06aa1316c0df4f
Manasa Putsala <manumanasap1@gmail.com>
2018-12-12 17:11:11 -0800

    Adding add_one()

--- ex.py
+++ ex.py
@@ -4,8 +4,14 @@ def count_sum(n):
         total += i
     return total
 
+def add_one(a):
+    return a + 1
+
 assert count_sum(10) == 55
 assert count_sum(1) == 1
 assert count_sum(0) == 0
+
+assert add_one(10) == 11
+assert add_one(0) == 1
 
 print "All tests passed"
