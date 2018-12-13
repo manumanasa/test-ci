0df0ce28dd263666b2c3526dce58b7776ca58ba0
Manasa Putsala <manumanasap1@gmail.com>
2018-12-12 16:50:09 -0800

    Initial commit.

---
+++ ex.py
@@ -0,0 +1,11 @@
+def count_sum(n):
+    total = 0
+    for i in range(n+1):
+        total += i
+    return total
+
+assert count_sum(10) == 55
+assert count_sum(1) == 1
+assert count_sum(0) == 0
+
+print "All tests passed"
